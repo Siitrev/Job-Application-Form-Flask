07eda5e9d0695137b691abff25a9a248c77d1177
Siitrev <paplata50@gmail.com>
2023-04-14 15:08:49 +0200

    Refactor

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from flask_mail import Mail, Message
 from utils.constants import SECRET_KEY, MAIL_SERVER, MAIL_USERNAME, MAIL_PASSWORD, MAIL_PORT
 from datetime import datetime
 
+# Create app instance
 app = Flask(__name__)
 
-
+# Set config
 app.config["SECRET_KEY"] = SECRET_KEY
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["MAIL_SERVER"] = MAIL_SERVER
@@ -15,11 +16,13 @@ app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USERNAME"] = MAIL_USERNAME
 app.config["MAIL_PASSWORD"] = MAIL_PASSWORD
 
+# Create database instance
 db = SQLAlchemy(app)
 
+# Create mail instance
 mail = Mail(app)
 
-
+# Create a table
 class Form(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80))
@@ -28,11 +31,13 @@ class Form(db.Model):
     date = db.Column(db.Date)
     occupation = db.Column(db.String(80))
 
-
+# Create home page
 @app.route("/", methods = ["GET","POST"])
 def index():
+    # Handle request
     if request.method == "POST":
         
+        # Get form data
         first_name = request.form["first_name"]
         last_name = request.form["last_name"]
         email = request.form["email"]
@@ -40,6 +45,7 @@ def index():
         date_obj = datetime.strptime(date,"%Y-%m-%d")
         occupation = request.form["occupation"]
         
+        # Add data to database
         form = Form(first_name=first_name,
                     last_name=last_name,
                     email=email,
@@ -48,6 +54,7 @@ def index():
         db.session.add(form)
         db.session.commit()
         
+        # Prepare message
         message_body = f"""Thank you for your submission, {first_name}.
         Here are your data: 
         {first_name}
@@ -60,8 +67,10 @@ def index():
                           recipients=[email],
                           body=message_body)
         
+        # Send email
         mail.send(message)
         
+        # Show confirmation alert
         flash(f"{first_name}, your form was submitted successfully!", "success")
         
     return render_template("index.html")
@@ -69,4 +78,4 @@ def index():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        app.run(debug=True, port=5001)
+        app.run(port=5001)
--- utils/constants.py
+++ utils/constants.py
@@ -1,9 +1,11 @@
 import environ
 import os
 
+# Read env
 env = environ.Env()
 env.read_env("static/.env")
 
+# Get variables
 SECRET_KEY = os.environ.get("SECRET_KEY")
 MAIL_SERVER = os.environ.get("MAIL_SERVER")
 MAIL_PORT = os.environ.get("MAIL_PORT")
